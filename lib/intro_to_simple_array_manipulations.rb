3b8bcabd7615360aa41de24911fcf80ead5acc58
Yu Shi <yushiys95@gmail.com>
2021-01-19 03:36:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array)
+array.concat
+end
+
+def using_insert(array)
+array.insert
+end
+
+def using_uniq(array)
+array.uniq
+end
+
+def using_flatten(array)
+array.flatten
+end
+
+def using_delete(array)
+array.delete
+end
+
+def using_delete_at(array)
+array.delete.at
+end
