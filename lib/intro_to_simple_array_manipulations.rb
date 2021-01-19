4448e4580913bf2565c4ff8c452a7dc73e293922
Yu Shi <yushiys95@gmail.com>
2021-01-19 03:44:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,10 +14,10 @@ def using_flatten(array)
 array.flatten
 end
 
-def using_delete(array)
-array.delete
+def using_delete(array,element)
+array.delete(element)
 end
 
-def using_delete_at(array)
-array.delete.at
+def using_delete_at(array,element)
+array.delete_at(element)
 end
