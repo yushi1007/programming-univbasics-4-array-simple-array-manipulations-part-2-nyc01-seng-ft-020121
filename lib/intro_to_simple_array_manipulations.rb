4686ba0c3ebada7e7492b52e57d3830b83499c11
Yu Shi <yushiys95@gmail.com>
2021-01-19 03:45:30 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_concat(array,element)
+array.concat(element)
+end
+
+def using_insert(array, element)
+array.insert(4, element)
+end
+
+def using_uniq(array)
+array.uniq
+end
+
+def using_flatten(array)
+array.flatten
+end
+
+def using_delete(array,element)
+array.delete(element)
+end
+
+def using_delete_at(array,element)
+array.delete_at(element)
+end
